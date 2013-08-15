743b74e85c54e402d76331fedcf9055c75f64c7e
Joe Dytrych <me@jdytrych.co.uk>
2013-08-15 17:15:09 +0100

    Added hacktionary definitions when clicking tag names

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,81 @@ $(function() {
     $el.before($button);
   })
 });
+
+/*
+
+  Include the definitions from the Hacktionary (https://github.com/brianloveswords/hacktionary)
+  in Prism's syntax highlighting. When a token is clicked, a popover with the definition is
+  shown. Based on the Web Platform Docs plugin: https://github.com/LeaVerou/prism/blob/gh-pages/plugins/wpd/
+
+*/
+
+(function() {
+  "use strict";
+
+  if (!self.Prism || !self.Hacktionary) {
+    return;
+  }
+
+  var entityMap = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': '&quot;',
+    "'": '&#39;',
+    "/": '&#x2F;'
+  };
+
+  function escapeHtml(string) {
+    return String(string).replace(/[&<>"'\/]/g, function (s) {
+      return entityMap[s];
+    });
+  }
+
+  if (Prism.languages.markup) {
+    Prism.languages.markup.tag.inside.tag.inside['tag-id'] = /[\w-]+/;
+  }
+
+  Prism.hooks.add("wrap", function(env) {
+    if (env.language === "markup") {
+      var el = Hacktionary.htmlElements[env.content.toLowerCase()] || false;
+
+      if (el !== false && el.desc !== undefined) {
+        env.tag = "a";
+        env.attributes.href = "#";
+        env.attributes["data-hacktionary"] = "htmlElements." + env.content.toLowerCase() + ".desc";
+      }
+    }
+  });
+
+  $(document).on("click", "[data-hacktionary]", function(e) {
+    e.preventDefault();
+    var $el = $(this),
+      desc = $el.attr("data-hacktionary").split("."),
+      ref = Hacktionary;
+
+    for (var i = 0; i < desc.length; i++) {
+      ref = ref[desc[i]];
+    }
+
+    if ($el.data("bs.popover") === undefined) {
+      $el.popover({
+        html: true,
+        content: ref,
+        container: "body",
+        placement: "bottom"
+      });
+
+      $el.popover("show");
+    } else {
+      $el.popover("destroy");
+    }
+
+    $(document).one("click", function() {
+      $el.popover("destroy");
+    });
+
+  });
+
+})();
+
