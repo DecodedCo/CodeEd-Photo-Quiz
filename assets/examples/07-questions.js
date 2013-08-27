e91bbb326785948b0088c9c5ffaad9a1c43bc2ea
Joe Dytrych <me@jdytrych.co.uk>
2013-08-27 11:21:08 +0100

    Added JSON section

---
+++ assets/examples/07-questions.js
@@ -0,0 +1,12 @@
+[
+  {
+    "question": "Which of these is known as 'the first programmer'",
+    "answer": "http://upload.wikimedia.org/wikipedia/commons/0/0f/Ada_lovelace.jpg",
+    "theme": "19th Century Lady"
+  },
+  {
+    "question": "Who invented the world wide web?",
+    "answer": "http://upload.wikimedia.org/wikipedia/commons/8/83/Tim_Berners-Lee-Knight-crop.jpg",
+    "theme": "computer scientist"
+  }
+]
---
+++ assets/examples/07-quiz.js
@@ -0,0 +1,11 @@
+function loadQuiz() {
+  // get the file questions.json, and call displayQuiz when done
+  $.get("questions.json", displayQuiz);
+}
+
+function displayQuiz(questions) {
+  console.log(questions);
+}
+
+// When the document is ready, call loadQuiz
+$(document).on("ready", loadQuiz);
