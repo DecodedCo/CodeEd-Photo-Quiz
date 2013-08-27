d1071ddafe743608ca00685731a7b2ae49d035fc
Joe Dytrych <me@jdytrych.co.uk>
2013-08-27 20:33:39 +0100

    Describe rendering templates onto the page

---
+++ assets/examples/08-js.js
@@ -0,0 +1,30 @@
+function displayQuiz(questions) {
+  // Find the list of questions with a CSS selector
+  var questionList = $("ol");
+
+  // Make a Handlebars template from the tag in our HTML
+  var template = Handlebars.compile($("#question").html());
+
+  // Loop through all the questions
+  for (var i = 0; i < questions.length; i++) {
+    var answer = questions[i].answer;
+
+    // Make an object to represent the question for the template
+    var data = {
+      question: questions[i].question,
+      images: [
+        {
+          url: answer,
+          correct: true
+        },
+        {
+          url: answer,
+          correct: false
+        }
+      ]
+    };
+
+    // Render the question with the template, and add it to the document
+    questionList.html(questionList.html() + template(data));
+  }
+}
